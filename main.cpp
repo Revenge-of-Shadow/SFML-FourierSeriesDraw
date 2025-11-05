f4d9d62b3a903b888bce7df48e5bf223be19d74d
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-11-05 23:51:08 +0100

    01: After hours of obsessive experiments I decided to upload it.

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <cstdint>
+#include <ctime>
+
+
+const double pi = std::acos(-1);
+
+class Pen{
+public:
+  float r;
+  float omega;
+
+  Pen(){}
+  Pen(float r, float o): r(r), omega(o){}
+};
+
+int main()
+{
+  std::srand(std::time({}));
+
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "nya");
+
+    
+  int max_t = INT16_MAX;
+  int r = 100;
+  const int arrlen = std::rand()%10;
+  Pen pens[arrlen];
+
+  for(int i = 0; i < arrlen; ++i){
+    pens[i] = Pen((std::rand()%r), (std::rand()%r-r/2));
+  }
+
+  while(window.isOpen()){
+    sf::Event event;
+    while(window.pollEvent(event)){
+      if(event.type == sf::Event::Closed)
+        window.close();
+    }
+    window.clear();
+
+    sf::VertexArray pts(sf::PrimitiveType::Points, max_t);
+
+    
+    for(int t = 0; t < max_t; ++t) {
+      //  Set starting point at the center.
+      sf::Vector2f pos = sf::Vector2f(window.getSize().x/2.f, window.getSize().y/2.f);
+      //  Move by the arrow thingies.
+      for(int i = 0; i<arrlen; ++i){
+        double angle = pi * 2.f / max_t * t * pens[i].omega  +  pi/2.f;
+        sf::Vector2f offs = sf::Vector2f(std::cos(angle)*pens[i].r, std::sin(angle)*pens[i].r);
+        pos += offs;
+      }
+      //  Set the final position as the drawing point.
+      pts[t].position = pos;
+    }
+    window.draw(pts);
+    window.display();
+  }
+}
